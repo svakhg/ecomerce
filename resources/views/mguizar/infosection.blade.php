3c7bc7451ab6d99067be65a3af8096df0a1fdff5
marqzosvaldo <crosser.xr@gmail.com>
2017-02-06 23:43:53 -0600

    footer-bank

--- resources/views/mguizar/infosection.blade.php
+++ resources/views/mguizar/infosection.blade.php
@@ -1,27 +1,82 @@
-<div class="col-md-12" id="info-section" style="text-align:center">
-    <div class="col-md-3">
-        <i class="fa fa-exchange fa-5x" ></i>
-        <h3>Paga y recibe</h3>
 
+<footer>
+    <div class="col-md-12" id="info-section">
+        <div class="col-md-3">
+            <div class="" style="text-align:center">
+                <i class="fa fa-exchange fa-5x fa-footer"style="" ></i>
+            </div>
+
+
+            <div class="row justify-content-sm-center" style="text-align:-webkit-center;text-align: -moz-center;" >
+                <h5 style="color:white;font-family:Ubuntu;" >Tarjetas de Crédito</h5>
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" style="text-align: -moz-center;text-align:-webkit-center;">
+                    <img class="img-responsive footer-icons" src="img/visa-icon.png" alt="">
+
+                </div>
+
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" style="text-align: -moz-center;text-align:-webkit-center;">
+                    <img class="img-responsive footer-icons" src="img/americanexpress.png" alt="">
+
+                </div>
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" style="text-align: -moz-center;text-align:-webkit-center;">
+                    <img class="img-responsive footer-icons" src="img/mastercard.png" alt="" style="margin-top:4px;">
+                </div>
+
+
+            </div>
+            <div class="row justify-content-sm-center" style="text-align:-webkit-center;text-align: -moz-center;">
+                <h5 style="color:white;font-family:Ubuntu; " >Tarjetas de Débito</h5>
+
+                <div class="col-xs-6 col-sm-4 col-md-12 col-lg-12">
+                    <p style="font-family: Ubuntu Condensed;color: thistle;" >Banamex, HSBC, Inbursa, Santander. </p>
+                </div>
+
+            </div>
+            <div class="row justify-content-sm-center" style="text-align:-webkit-center;text-align: -moz-center;">
+                <h5 style="color:white;font-family:Ubuntu; " >Pagos en Efectivo</h5>
+
+                <div class="col-xs-6 col-sm-4 col-md-12 col-lg-12">
+                    <p style="font-family: Ubuntu Condensed;color: thistle;" > OXXO y Banorte  </p>
+                </div>
+
+            </div>
+            <div class="row justify-content-sm-center" style="text-align:-webkit-center;text-align: -moz-center;">
+                <h5 style="color:white;font-family:Ubuntu; " >Meses sin Intereses*</h5>
+
+                <div class="col-xs-6 col-sm-4 col-md-12 col-lg-12">
+                    <p style="text-align: justify;font-family: Ubuntu Condensed;color: thistle;"> Afirme, American Express, Banamex, BanBajío, Banejercito, Banorte, Banca Mifel, Bancomer,
+                         Banregio, Famsa, HSBC, Itaucard, Inbursa, Invex, IXE, Santander, Platinum Card.  </p>
+                </div>
+
+            </div>
+
+
+        </div>
+
+        <div class="col-md-3">
+            <div class="" style="text-align:center">
+                <i class="fa fa-home fa-5x fa-footer" ></i>
+                <h5>Proyectos</h5>
+            </div>
+
+
+
+        </div>
+        <div class="col-md-3">
+            <i class="fa fa-envelope fa-5x fa-footer"></i>
+            <h5>Contáctanos</h5>
+
+
+        </div>
+        <div class="col-md-3">
+            <i class="fa fa-map-marker fa-5x fa-footer" ></i>
+            <h5 style="color:white;font-family:Ubuntu;">Como localizarnos</h5>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15283.335000906716!2d-93.11466!3d16.735145!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x968f1e04aaa75cb9!2sRefri+Comercial+Marin+Guizar+S.A.+de+C.V.!5e0!3m2!1ses-419!2smx!4v1486195584848" width="auto" height="auto" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+
+        </div>
 
     </div>
-    <div class="col-md-3">
-        <i class="fa fa-map-marker fa-5x" ></i>
-        <h3>Como localizarnos</h3>
 
 
-    </div>
-    <div class="col-md-3">
-        <i class="fa fa-home fa-5x" ></i>
-        <h3>Proyectos</h3>
-
-
-    </div>
-    <div class="col-md-3">
-        <i class="fa fa-envelope fa-5x"></i>
-        <h3>Contáctanos</h3>
-
-
-    </div>
-
-</div>
+</footer>
